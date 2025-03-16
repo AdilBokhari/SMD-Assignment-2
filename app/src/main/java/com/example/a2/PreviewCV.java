3a85ff25df30f8f4e2a3b10c44d519706b7cb01f
Adil <bokhariadil@gmail.com>
2025-03-16 14:33:51 +0500

    Share CV

--- app/src/main/java/com/example/a2/PreviewCV.java
+++ app/src/main/java/com/example/a2/PreviewCV.java
@@ -3,6 +3,7 @@ package com.example.a2;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class PreviewCV extends AppCompatActivity {
 
     ImageView ivProfilePic;
     TextView tvName,tvEmail,tvPhoneNo,tvSummary,tvEducation,tvExperience,tvCert,tvRef;
+    Button btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,38 +51,71 @@ public class PreviewCV extends AppCompatActivity {
         {
             ivProfilePic.setImageResource(R.drawable.def);
         }
+
+        StringBuilder CV=new StringBuilder();
+
         if(name!=null && !name.isEmpty())
         {
             tvName.setText(name);
+            CV.append("Name: ");
+            CV.append(name);
+            CV.append("\n");
         }
         if(email!=null && !email.isEmpty())
         {
             tvEmail.setText(email);
+            CV.append("\nEmail: ");
+            CV.append(email);
+            CV.append("\n");
         }
         if(phoneNo!=null && !phoneNo.isEmpty())
         {
             tvPhoneNo.setText(phoneNo);
+            CV.append("\nPhone: ");
+            CV.append(phoneNo);
+            CV.append("\n");
         }
         if(summary!=null && !summary.isEmpty())
         {
             tvSummary.setText(summary);
+            CV.append("\nSummary:\n");
+            CV.append(summary);
+            CV.append("\n");
         }
         if(education!=null && !education.isEmpty())
         {
             tvEducation.setText(education);
+            CV.append("\nEducation:\n");
+            CV.append(education);
+            CV.append("\n");
         }
         if(experience!=null && !experience.isEmpty())
         {
             tvExperience.setText(experience);
+            CV.append("\nExperience:\n");
+            CV.append(experience);
+            CV.append("\n");
         }
         if(cert!=null && !cert.isEmpty())
         {
             tvCert.setText(cert);
+            CV.append("\nCertifications:\n");
+            CV.append(cert);
+            CV.append("\n");
         }
         if(ref!=null && !ref.isEmpty())
         {
             tvRef.setText(ref);
+            CV.append("\nReferences:\n");
+            CV.append(ref);
+            CV.append("\n");
         }
+        btnShare.setOnClickListener((v)->{
+            Intent intent=new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT,CV.toString());
+            intent.setType("text/*");
+            startActivity(intent);
+        });
     }
     private void init()
     {
@@ -93,5 +128,6 @@ public class PreviewCV extends AppCompatActivity {
         tvExperience=findViewById(R.id.tvExperience);
         tvCert=findViewById(R.id.tvCert);
         tvRef=findViewById(R.id.tvRef);
+        btnShare=findViewById(R.id.btnShare);
     }
 }
